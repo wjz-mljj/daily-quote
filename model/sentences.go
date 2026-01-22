c9abd353c94675ca7b50f1e6a36b4fffab413d31
wjz <w492597745@163.com>
2026-01-22 14:30:31 +0800

    1

--- model/sentences.go
+++ model/sentences.go
@@ -2,9 +2,14 @@ package model
 
 import "gorm.io/gorm"
 
+// gorm.Model 的定义：https://gorm.io/zh_CN/docs/models.html
+
+// Sentence 句子模型
 type Sentence struct {
 	gorm.Model
-	Content string `json:"content"`
+	Content         string  `json:"content"`
+	Type            *string `json:"type"`             // 句子分析类型， *表示可选字段
+	AnalysisResults *string `json:"analysis_results"` // JSON string of analysis results
 }
 
 type PageResult[T any] struct {
